d3c5496bdfa4c47d141aa6dad77a3043053ad68f
Juanfco0128 <Juanfco0128@gmail.com>
2019-11-06 00:07:52 +0000

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,13 @@ import "../style/index.scss";
 window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
+let suit = ["♥", "♦", "♣", "♠"];
+let number = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let suit2 = suit[Math.floor(Math.random() * suit.length)];
+
+document.querySelector(".suit").innerHTML = suit2;
+
+document.querySelector(".number").innerHTML =
+  number[Math.floor(Math.random() * number.length)];
+
+document.querySelector(".suit2").innerHTML = suit2;
